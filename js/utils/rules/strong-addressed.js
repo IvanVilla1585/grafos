a54b39814232c10c174ddebfad51b311f2574d1a
Ivan Villa <trezeguet55@gmail.com>
2018-05-15 21:07:31 -0500

    <js rules strong-addressed.js>
    create functionality

---
+++ js/utils/rules/strong-addressed.js
@@ -0,0 +1,29 @@
+
+import FloydWarshall from "floyd-warshall";
+
+export function strong(graph) {
+  let valid = true
+  const warshal = new FloydWarshall(graph.matriz)
+  const result = warshal.widestPaths
+
+  result.map((_row, i) => {
+    _row.map((_d, j) => {
+      if (i !== j && _row[j] === 0) {
+        valid = valid && false
+      }
+    })
+  })
+
+  return valid
+}
+
+export function countSourceTargetGrades(links, circles) {
+  const data = circles.map(_circle => {
+    const _entries = links.filter(_link => _link.source === _circle.id)
+    const _targets = links.filter(_link => _link.target === _circle.id)
+    _circle.entries = _entries.length
+    _circle.targets = _targets.length
+    return _circle
+  })
+  return data
+}
