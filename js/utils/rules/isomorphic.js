f76c7cc904378ecee7335c97575c3db68fe9b8e7
Ivan Villa <trezeguet55@gmail.com>
2018-05-17 23:15:10 -0500

    <js rules isomorphic.js>
    fix bugs standard

--- js/utils/rules/isomorphic.js
+++ js/utils/rules/isomorphic.js
@@ -1,8 +1,8 @@
 'use strict'
 
 export const isomorphic = (graphs) => {
-  const {graph_1, graph_2} = graphs
-  const data = composeData(graph_1, graph_2)
+  const {graph1, graph2} = graphs
+  const data = composeData(graph1, graph2)
   return data
 }
 
@@ -25,7 +25,7 @@ const composeData = (graph1, graph2) => {
   return data
 }
 
-function validatorNodesEdges(array1, array2, name1, name2, key, data) {
+function validatorNodesEdges (array1, array2, name1, name2, key, data) {
   if (array1.length === array2.length) {
     const dataKey = key === 'nodes' ? 'circles' : 'links'
     if (key === 'nodes') {
@@ -33,13 +33,13 @@ function validatorNodesEdges(array1, array2, name1, name2, key, data) {
     } else {
       data.isEdgesEquals = true
     }
-    data.graph_1 = {
-      ...data.graph_1,
+    data.graph1 = {
+      ...data.graph1,
       [key]: array1.length,
       [dataKey]: array1
     }
-    data.graph_2 = {
-      ...data.graph_2,
+    data.graph2 = {
+      ...data.graph2,
       [key]: array2.length,
       [dataKey]: array2
     }
@@ -54,17 +54,17 @@ function validatorNodesEdges(array1, array2, name1, name2, key, data) {
   return {...data}
 }
 
-function validatorGrades(name1, name2, data) {
-  const {graph_1, graph_2} = data
-  graph_1.circles = countGrades(graph_1.links, graph_1.circles)
-  graph_2.circles = countGrades(graph_2.links, graph_2.circles)
-  const result = compareGrades(graph_1.circles, graph_2.circles)
+function validatorGrades (name1, name2, data) {
+  const {graph1, graph2} = data
+  graph1.circles = countGrades(graph1.links, graph1.circles)
+  graph2.circles = countGrades(graph2.links, graph2.circles)
+  const result = compareGrades(graph1.circles, graph2.circles)
   if (result.valid) {
     data.isNodesGradesEquals = true
   } else {
     data.isNodesGradesEquals = false
-    const circle1 = graph_1.circles[result.pos]
-    const circle2 = graph_2.circles[result.pos]
+    const circle1 = graph1.circles[result.pos]
+    const circle2 = graph2.circles[result.pos]
     data.errorMessage = `
       No es un grafo isomorfo por que el vertice ${circle1.text} de los 2 grafos no tienen el mismo grado,
       vertice ${circle1.text} con grado ${circle1.grade} del ${name1} y vertice ${circle2.text} con grado ${circle2.grade} del ${name2} 
@@ -73,16 +73,16 @@ function validatorGrades(name1, name2, data) {
 
   return {
     ...data,
-    graph_1: {
-      ...graph_1
+    graph1: {
+      ...graph1
     },
-    graph_2: {
-      ...graph_2
+    graph2: {
+      ...graph2
     }
   }
 }
 
-export function countGrades(links, circles) {
+export function countGrades (links, circles) {
   const data = circles.map(_circle => {
     const _edges = links.filter(_link => _link.source === _circle.id || _link.target === _circle.id)
     _circle.grade = _edges.length
@@ -91,7 +91,7 @@ export function countGrades(links, circles) {
   return data
 }
 
-function compareGrades(circles1, circles2) {
+function compareGrades (circles1, circles2) {
   let valid = true
   let pos = -1
   circles1.map((_circle, index) => {
@@ -99,7 +99,6 @@ function compareGrades(circles1, circles2) {
     if (_circle.grade !== _c.grade) {
       valid = false
       pos = index
-      return false;
     }
   })
   return {valid, pos}
